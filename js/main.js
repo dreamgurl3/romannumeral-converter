b514b6402a9a44c6f9d7c6fd7270f9334482b09e
dreamgurl3 <jessie.0507@gmail.com>
2024-08-20 16:25:03 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const convertBtn = document.getElementById("convert");
+const numberInput = document.getElementById("number");
+const conversion = document.getElementById("conversionHere");
+
+const validateInput = () => {
+    const inputVal = parseInt(numberInput.value);
+
+    if (isNaN(inputVal) || inputVal < 1 || inputVal > 3999) {
+        conversion.innerText = "Please enter a valid number between 1 and 3999.";
+        return false;
+    }
+    conversion.innerText = decimalToRoman(inputVal);
+    return true;
+};
+const decimalToRoman = (num) => {
+    const romanNumerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    const decimalVals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let result = "";
+    for (let i = 0; i < decimalVals.length; i++) {
+        while (num >= decimalVals[i]) {
+            result += romanNumerals[i];
+            num -= decimalVals[i];
+        }
+    }
+
+    return result;
+};
+convertBtn.addEventListener("click", validateInput);
+numberInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        validateInput();
+    }
+});
+
+
+
+
