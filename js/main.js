762243dde911112e63e235b1d0b1dd4750929d53
dreamgurl3 <jessie.0507@gmail.com>
2024-09-21 20:51:41 -0500

    third commit

--- js/main.js
+++ js/main.js
@@ -2,16 +2,6 @@ const convertBtn = document.getElementById("convert");
 const numberInput = document.getElementById("number");
 const conversion = document.getElementById("conversionHere");
 
-const validateInput = () => {
-    const inputVal = parseInt(numberInput.value);
-
-    if (!numberInput.value || isNaN(inputVal) || inputVal < 1 || inputVal > 3999) {
-        conversion.innerText = "Please enter a valid number between 1 and 3999.";
-        return;
-    }
-    conversion.innerText = decimalToRoman(inputVal);
-    return;
-};
 const decimalToRoman = (num) => {
     const romanNumerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
     const decimalVals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
@@ -25,6 +15,18 @@ const decimalToRoman = (num) => {
 
     return result;
 };
+
+const validateInput = () => {
+    const inputVal = parseInt(numberInput.value);
+
+    if (!numberInput.value || isNaN(inputVal) || inputVal < 1 || inputVal > 3999) {
+        conversion.innerText = "Please enter a valid number between 1 and 3999.";
+        return;
+    }
+    conversion.innerText = decimalToRoman(inputVal);
+    return;
+};
+
 convertBtn.addEventListener("click", validateInput);
 numberInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
